7e1710d46e85fbc5f086f9a5efb108c3a6b318bd
Shane <Shane@Tesss-Personal-MacBook-Pro.local>
2023-02-15 07:38:12 -0800

    Made gpio passing generic

--- ch02-blinky-thread/src/main.rs
+++ ch02-blinky-thread/src/main.rs
@@ -1,6 +1,5 @@
-use esp_idf_hal::gpio;
 use esp_idf_hal::{
-    gpio::{Output, PinDriver},
+    gpio::{AnyOutputPin, Output, OutputPin, PinDriver},
     prelude::*,
 };
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
@@ -14,7 +13,7 @@ fn main() {
     esp_idf_sys::link_patches();
 
     let peripherals = Peripherals::take().unwrap();
-    let led = PinDriver::output(peripherals.pins.gpio8).unwrap();
+    let led = PinDriver::output(peripherals.pins.gpio8.downgrade_output()).unwrap();
     // print_type_of(&led);
 
     let _blinky_thread = std::thread::Builder::new()
@@ -23,7 +22,7 @@ fn main() {
         .unwrap();
 }
 
-fn blinky_thread(mut led: PinDriver<'_, gpio::Gpio8, Output>) {
+fn blinky_thread(mut led: PinDriver<AnyOutputPin, Output>) {
     loop {
         thread::sleep(Duration::from_millis(500));
         println!("LED ON");
