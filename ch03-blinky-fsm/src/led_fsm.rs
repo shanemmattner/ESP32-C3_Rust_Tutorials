f4d46e6bf884d8b998feb8f521e40a7526f8f6c5
Shane <Shane@Tesss-Personal-MacBook-Pro.local>
2023-02-19 08:23:17 -0800

    refactor ch03

--- ch03-blinky-fsm/src/led_fsm.rs
+++ ch03-blinky-fsm/src/led_fsm.rs
@@ -21,15 +21,15 @@ pub enum Event {
 
 #[derive(Debug)]
 pub enum State {
-    On,
-    Off,
+    LedOn,
+    LedOff,
 }
 
 impl StateMachine for Blinky<'_> {
     type State = State;
     type Superstate<'a> = ();
     type Event<'a> = Event;
-    const INITIAL: State = State::Off;
+    const INITIAL: State = State::LedOff;
     const ON_TRANSITION: fn(&mut Self, &Self::State, &Self::State) = |_, source, target| {
         println!("Transitioned from {source:?} to {target:?}");
     };
@@ -38,24 +38,38 @@ impl StateMachine for Blinky<'_> {
 impl statig::State<Blinky<'_>> for State {
     fn call_handler(&mut self, blinky: &mut Blinky, event: &Event) -> Response<Self> {
         match self {
-            State::On => blinky.on(event),
-            State::Off => blinky.off(event),
+            State::LedOn => blinky.led_on(event),
+            State::LedOff => blinky.led_off(event),
+        }
+    }
+
+    fn call_entry_action(&mut self, blinky: &mut Blinky) {
+        match self {
+            State::LedOn => blinky.enter_led_on(),
+            State::LedOff => blinky.enter_led_off(),
         }
     }
 }
 
 impl Blinky<'_> {
-    fn on(&mut self, event: &Event) -> Response<State> {
-        // Here we are transitioning to the off state
+    fn enter_led_on(&mut self) {
         // Setting the pin high turns the LED off on my dev board
+        self.led.set_low().unwrap();
+    }
+    fn enter_led_off(&mut self) {
+        // Setting the pin low turns the LED on for my dev board
         self.led.set_high().unwrap();
-        Transition(State::Off)
     }
 
-    fn off(&mut self, event: &Event) -> Response<State> {
-        // Here we are transitioning to the on state
-        // Setting the pin low turns the LED on for my dev board
-        self.led.set_low().unwrap();
-        Transition(State::On)
+    fn led_on(&mut self, event: &Event) -> Response<State> {
+        match event {
+            Event::TimerElapsed => Transition(State::LedOff),
+        }
+    }
+
+    fn led_off(&mut self, event: &Event) -> Response<State> {
+        match event {
+            Event::TimerElapsed => Transition(State::LedOn),
+        }
     }
 }
