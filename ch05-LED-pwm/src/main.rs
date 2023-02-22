a31bb6598bc5530500698c458538b5af782686ff
Shane <shanemmattner@gmail.com>
2023-02-21 18:34:51 -0800

    working example

--- ch05-LED-pwm/src/main.rs
+++ ch05-LED-pwm/src/main.rs
@@ -1,15 +1,21 @@
 use crossbeam_channel::bounded;
+use crossbeam_utils::atomic::AtomicCell;
 use esp_idf_hal::{
-    gpio::{AnyIOPin, AnyOutputPin, IOPin, Input, Output, OutputPin, PinDriver, Pull},
+    adc::{self, *},
+    gpio::{ADCPin, AnyIOPin, AnyOutputPin, IOPin, Input, Output, OutputPin, PinDriver, Pull},
+    ledc::{config::TimerConfig, *},
+    peripherals::Peripherals,
     prelude::*,
 };
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use esp_println::println;
-use std::{thread, time::Duration};
+use std::{sync::Arc, thread, time::Duration};
 
 static BLINKY_STACK_SIZE: usize = 2000;
 static BUTTON_STACK_SIZE: usize = 2000;
 static ADC_STACK_SIZE: usize = 2000;
+
+static ADC_MAX_COUNTS: u32 = 2850;
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -17,15 +23,38 @@ fn main() {
     esp_idf_sys::link_patches();
 
     let peripherals = Peripherals::take().unwrap();
-    let led_pin = PinDriver::output(peripherals.pins.gpio8.downgrade_output()).unwrap();
+    let led_pin = PinDriver::output(peripherals.pins.gpio4.downgrade_output()).unwrap();
     let mut btn_pin = PinDriver::input(peripherals.pins.gpio6.downgrade()).unwrap();
-    btn_pin.set_pull(Pull::Down).unwrap();
+    btn_pin.set_pull(Pull::Up).unwrap();
 
+    // Crossbeam channel
     let (tx, rx) = bounded(1);
 
+    // ADC init
+    // Create atomic to store adc readings
+    let atomic_value: AtomicCell<u16> = AtomicCell::new(0);
+    let arc_data = Arc::new(atomic_value);
+    // Create ADC channel driver
+    let a1_ch0 =
+        adc::AdcChannelDriver::<_, adc::Atten11dB<adc::ADC1>>::new(peripherals.pins.gpio0).unwrap();
+    let adc1 = AdcDriver::new(
+        peripherals.adc1,
+        &adc::config::Config::new().calibration(true),
+    )
+    .unwrap();
+
+    let timer_config = TimerConfig::new().frequency(25.kHz().into());
+    let timer = Arc::new(LedcTimerDriver::new(peripherals.ledc.timer0, &timer_config).unwrap());
+    let channel_0 = LedcDriver::new(
+        peripherals.ledc.channel0,
+        timer.clone(),
+        peripherals.pins.gpio8,
+    )
+    .unwrap();
+    let a1 = arc_data.clone();
     let _blinky_thread = std::thread::Builder::new()
         .stack_size(BLINKY_STACK_SIZE)
-        .spawn(move || blinky_thread(led_pin, rx))
+        .spawn(move || blinky_thread(led_pin, rx, a1, channel_0))
         .unwrap();
 
     let _button_thread = std::thread::Builder::new()
@@ -33,22 +62,62 @@ fn main() {
         .spawn(move || button_thread(btn_pin, tx))
         .unwrap();
 
+    let a2 = arc_data.clone();
     let _adc_thread = std::thread::Builder::new()
-        .stack_size(ADC_STACK_SIZE.spawn(move || button_thread(btn_pin, tx))
-        .spawn(move || adc_thread(btn_pin, tx))
+        .stack_size(ADC_STACK_SIZE)
+        .spawn(move || adc_thread(a2, adc1, a1_ch0))
+        .unwrap();
+}
+
+fn adc_thread<T: ADCPin>(
+    adc_mutex: Arc<AtomicCell<u16>>,
+    mut adc: AdcDriver<adc::ADC1>,
+    mut pin: adc::AdcChannelDriver<T, adc::Atten11dB<adc::ADC1>>,
+) where
+    Atten11dB<ADC1>: Attenuation<<T as ADCPin>::Adc>,
+{
+    loop {
+        // Read ADC and and set the LED PWM to the percentage of full scale
+        match adc.read(&mut pin) {
+            Ok(x) => {
+                adc_mutex.store(x);
+            }
+
+            Err(e) => println!("err reading ADC: {e}\n"),
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    }
 }
 
 // Thread function that will blink the LED on/off every 500ms
 fn blinky_thread(
     mut led_pin: PinDriver<AnyOutputPin, Output>,
     rx: crossbeam_channel::Receiver<bool>,
+    adc_mutex: Arc<AtomicCell<u16>>,
+    mut channel: LedcDriver<'_>,
 ) {
     let mut blinky_status = false;
     loop {
+        // Watch for button press messages
         match rx.try_recv() {
-            Ok(x) => blinky_status = x,
+            Ok(x) => {
+                blinky_status = x;
+                // When we receive a button press then change the PWM of the pin
+                if blinky_status {
+                    let val = adc_mutex.load();
+                    let max_duty = channel.get_max_duty();
+                    let pwm = (val as u32 * max_duty) / ADC_MAX_COUNTS;
+                    match channel.set_duty(pwm) {
+                        Ok(_x) => (),
+                        Err(e) => println!("err setting duty of led: {e}\n"),
+                    }
+                }
+            }
             Err(_) => {}
         }
+
+        // blinky if the button was pressed
         if blinky_status {
             led_pin.set_low().unwrap();
             println!("LED ON");
@@ -58,6 +127,7 @@ fn blinky_thread(
             println!("LED OFF");
             thread::sleep(Duration::from_millis(1000));
         }
+
         thread::sleep(Duration::from_millis(100));
     }
 }
