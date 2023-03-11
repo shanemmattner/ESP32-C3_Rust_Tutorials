d44138c8cb598ba5f494a0fd403a8621c2a3628c
Shane <shanemmattner@gmail.com>
2023-03-11 09:22:57 -0800

    kinda working

--- Tutorials/p9-adc-stream/src/main.rs
+++ Tutorials/p9-adc-stream/src/main.rs
@@ -1,11 +1,12 @@
 mod cli;
+mod adc_driver;
+
 
 use esp_idf_hal::{delay::FreeRtos, adc, gpio::{self,AnyIOPin, AnyOutputPin, IOPin, Input, Output, OutputPin, PinDriver, Pull}, peripherals::Peripherals, prelude::*, uart};
 use esp_idf_sys as _;
 
 static CLI_STACK_SIZE: usize = 5000;
 static ADC_STACK_SIZE: usize = 5000;
-const ADC_CHANNEL_SIZE:usize = 4;
 
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
@@ -31,25 +32,46 @@ fn main() -> anyhow::Result<()> {
         .unwrap();
 
 
-    // ADC init
-    let mut adc1 = adc::AdcDriver::new(
+    // Create ADC channel driver
+    let a1_ch0 =
+        adc::AdcChannelDriver::<_, adc::Atten11dB<adc::ADC1>>::new(peripherals.pins.gpio0).unwrap();
+    let adc1 = adc::AdcDriver::new(
         peripherals.adc1,
         &adc::config::Config::new().calibration(true),
     )
     .unwrap();
 
-    let mut a1_ch0 =
-        adc::AdcChannelDriver::<_, adc::Atten11dB<adc::ADC1>>::new(peripherals.pins.gpio0).unwrap();
-    let mut a1_ch2 =
-    adc::AdcChannelDriver::<_, adc::Atten11dB<adc::ADC1>>::new(peripherals.pins.gpio2).unwrap();
-    let mut a1_ch3 =
-    adc::AdcChannelDriver::<_, adc::Atten11dB<adc::ADC1>>::new(peripherals.pins.gpio3).unwrap();
-    let mut a1_ch4 =
-    adc::AdcChannelDriver::<_, adc::Atten11dB<adc::ADC1>>::new(peripherals.pins.gpio4).unwrap();
+    // print_type_of(&peripherals.pins.gpio0);
 
-    let adc_channels:[adc::AdcChannelDriver<_, adc::Atten11dB<adc::ADC1>>;ADC_CHANNEL_SIZE] = [a1_ch0,a1_ch2,a1_ch3,a1_ch4];
+    let mut test = adc_driver::adc_stream_t {
+        adc :  adc1,
+        pin :  a1_ch0
+    };
+    match test.adc.read(&mut test.pin)
+    {
+        Ok(x) => {
+            println!("reading: {x}\n");
+        }
+
+        Err(e) => println!("err reading ADC: {e}\n"),
+        }
+    // adc_driver::adc_init(adc1,a1_ch0);
+    // let mut a1_ch0 =
+    //     adc::AdcChannelDriver::<_, adc::Atten11dB<adc::ADC1>>::new(peripherals.pins.gpio0).unwrap();
+    // let mut a1_ch2 =
+    // adc::AdcChannelDriver::<_, adc::Atten11dB<adc::ADC1>>::new(peripherals.pins.gpio2).unwrap();
+    // let mut a1_ch3 =
+    // adc::AdcChannelDriver::<_, adc::Atten11dB<adc::ADC1>>::new(peripherals.pins.gpio3).unwrap();
+    // let mut a1_ch4 =
+    // adc::AdcChannelDriver::<_, adc::Atten11dB<adc::ADC1>>::new(peripherals.pins.gpio4).unwrap();
+
     
     loop {
         FreeRtos::delay_ms(100);
     }
+}
+
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
 }
